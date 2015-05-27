359d96eeabefa39ee2a09cc381ae6851e3c0f79f
Benjamin Reed <ranger@opennms.org>
2015-05-27 12:24:30 -0400

    get rid of Selenium stuff, we use it directly with webdriver

--- script/smoke.pl
+++ script/smoke.pl
@@ -18,17 +18,11 @@ use version;
 use OpenNMS::Release;
 
 use vars qw(
-	$SELENIUM_LOG
-
 	$SCRIPT
 	$XVFB_RUN
 	$JAVA
 
 	$NON_DESTRUCTIVE
-
-	$SELENIUM_JAR
-	$SELENIUM_HUB
-	$SELENIUM_WEBDRIVER
 );
 
 END {
@@ -45,9 +39,6 @@ if (not defined $SCRIPT or $SCRIPT eq '' or not -x $SCRIPT) {
 	exit(1);
 }
 
-$SELENIUM_LOG = IO::Handle->new();
-open($SELENIUM_LOG, '>', '/tmp/selenium.log') or die "Failed to open /tmp/selenium.log for writing: $!\n";
-
 delete $ENV{'DISPLAY'};
 
 if (not exists $ENV{'JAVA_HOME'} or not -d $ENV{'JAVA_HOME'}) {
@@ -63,30 +54,6 @@ if (not defined $XVFB_RUN or $XVFB_RUN eq "" or ! -x $XVFB_RUN) {
 
 $NON_DESTRUCTIVE = (exists $ENV{'NON_DESTRUCTIVE'} and $ENV{'NON_DESTRUCTIVE'});
 
-update_selenium_pids();
-
-$SELENIUM_JAR = get_selenium_jar();
-
-if (not defined $SELENIUM_JAR) {
-	die "Unable to locate selenium JAR file!";
-}
-
-if (not defined $SELENIUM_HUB) {
-	print "Selenium hub is not running; starting:\n";
-	$SELENIUM_HUB = nohup($JAVA, '-jar', $SELENIUM_JAR, '-role', 'hub');
-	print "done.\n";
-}
-
-if (not defined $SELENIUM_WEBDRIVER) {
-	print "Selenium webdriver is not running; starting:\n";
-	$SELENIUM_WEBDRIVER = nohup($JAVA, '-jar', $SELENIUM_JAR, '-role', 'webdriver', '-hub', 'http://localhost:4444/grid/register', '-port', '5556');
-	print "done.\n";
-}
-
-sleep(5);
-
-update_selenium_pids();
-
 my $m2_repo = File::Spec->catdir($ENV{'HOME'}, '.m2', 'repository');
 if ($NON_DESTRUCTIVE) {
 	print "Skipping repository cleanup, \$NON_DESTRUCTIVE is set.\n";
@@ -101,60 +68,7 @@ my $ret = $? >> 8;
 
 exit($ret);
 
-sub get_selenium_jar {
-	my $dir = dist_dir('OpenNMS-Release');
-	my $handle = IO::Handle->new();
-	opendir($handle, $dir) or die "unable to open $dir for reading: $!\n";
-	while (my $file = readdir($handle)) {
-		if ($file =~ /selenium.*\.jar$/) {
-			return File::Spec->catfile($dir, $file);
-		}
-	}
-	return undef;
-}
-
-sub update_selenium_pids {
-	my $proc = Proc::ProcessTable->new();
-	for my $p (@{$proc->table}) {
-		if ($p->cmndline =~ /selenium-server-standalone-.*.jar\s*.*-role\s*([^\s]*)/) {
-			if ($1 eq 'webdriver') {
-				$SELENIUM_WEBDRIVER = $p->pid;
-			} elsif ($1 eq 'hub') {
-				$SELENIUM_HUB = $p->pid;
-			}
-		}
-	}
-}
-
-sub nohup {
-	my @command = @_;
-	local $SIG{'HUP'} = 'IGNORE';
-	my $pid = fork;
-	if ($pid) {
-		# parent
-		return $pid;
-	} else {
-		# child
-		STDOUT->fdopen( $SELENIUM_LOG, 'w' ) or die "Failed to redirect STDOUT to /tmp/selenium.log: $!\n";
-		STDERR->fdopen( $SELENIUM_LOG, 'w' ) or die "Failed to redirect STDOUT to /tmp/selenium.log: $!\n";
-		setsid() or die "Cannot establish session id: $!\n";
-		system(@command) == 0 or die "Failed to run @command: $!\n";
-		exit(0);
-	}
-}
-
 sub clean_up {
-	if (defined $SELENIUM_HUB and $SELENIUM_HUB =~ /^\d+$/) {
-		print "- stopping Selenium hub... ";
-		kill('TERM', $SELENIUM_HUB);
-		print "done\n";
-	}
-	if (defined $SELENIUM_WEBDRIVER and $SELENIUM_WEBDRIVER =~ /^\d+$/) {
-		print "- stopping Selenium web driver... ";
-		kill('TERM', $SELENIUM_WEBDRIVER);
-		print "done\n";
-	}
-
 	# make sure everything is owned by non-root
 	if (defined $SCRIPT and -x $SCRIPT) {
 		my $smokedir = dirname(abs_path($SCRIPT));
