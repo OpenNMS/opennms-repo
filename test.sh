2658fd7c571928cfed2c5175282e1b21618230bc
Benjamin Reed <ranger@opennms.org>
2017-07-19 12:12:16 -0400

    handle args better

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ARGS="$@"
-if [ -z "$ARGS" ]; then
-	ARGS="install"
+ARGS=("$@")
+if [ "${#ARGS[@]}" -eq 0 ]; then
+	ARGS=("install")
 fi
-docker run -it --rm --name my-maven-project -v "$PWD":/opt/build -v "$HOME/.m2:/root/.m2" opennms/maven mvn $ARGS
+docker run -it --rm --name my-maven-project -v "$PWD":/opt/build -v "$HOME/.m2:/root/.m2" opennms/maven mvn "${ARGS[@]}"
