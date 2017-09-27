1b3548bdcf9668b227a24c370770f8ff5c7111a8
Benjamin Reed <ranger@opennms.org>
2017-09-27 12:09:32 -0400

    For Meridian >= 2017, skip rhel5/rhel6 sync

--- script/update-yum-repo.pl
+++ script/update-yum-repo.pl
@@ -84,6 +84,15 @@ my $platforms = read_properties(dist_file('OpenNMS-Release', 'platform.propertie
 my @platform_order = split(/\s*,\s*/, $platforms->{order_display});
 delete $platforms->{order_display};
 
+my @core_rpms = grep { /\bmeridian-core-/ } @RPMS;
+if (exists $core_rpms[0]) {
+	my $core_rpm = OpenNMS::Release::RPMPackage->new($core_rpms[0]);
+	if ($core_rpm->version()->version() >= 2017) {
+		print "! Meridian 2017 or newer package found. Skipping rhel5/rhel6 sync.\n";
+		@platform_order = grep { !/(centos5|centos6|rhel5|rhel6)/i } @platform_order;
+	}
+}
+
 if ($ALL) {
 	for my $repo (@{OpenNMS::Release::YumRepo->find_repos($BASE)}) {
 		if (not grep { $_ eq $repo->release } @sync_order) {
