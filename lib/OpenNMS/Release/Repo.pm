83aa39f198a44c88e1de9c44d6b02e18286bf4dc
Benjamin Reed <ranger@opennms.org>
2018-05-22 15:18:30 -0400

    exclude .bak directories

--- lib/OpenNMS/Release/Repo.pm
+++ lib/OpenNMS/Release/Repo.pm
@@ -146,9 +146,9 @@ sub copy {
 	my $dest_fs   = $self->_get_fs_for_path($repopath);
 
 	if (defined $source_fs and defined $dest_fs and $source_fs eq $dest_fs) {
-		system($RSYNC, "-aqrH", "--link-dest=" . $selfpath . "/", $selfpath . "/", $repopath . "/") == 0 or croak "failure while rsyncing: $!";
+		system($RSYNC, "-aqrH", "--exclude=*.bak", "--link-dest=" . $selfpath . "/", $selfpath . "/", $repopath . "/") == 0 or croak "failure while rsyncing: $!";
 	} else {
-		system($RSYNC, "-aqrH", $selfpath . "/", $repopath . "/") == 0 or croak "failure while rsyncing: $!";
+		system($RSYNC, "-aqrH", "--exclude=*.bak", $selfpath . "/", $repopath . "/") == 0 or croak "failure while rsyncing: $!";
 	}
 
 	return $repo;
