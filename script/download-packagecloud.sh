98e9990e3bf48bea021413630d693c4f33fb3276
Benjamin Reed <ranger@opennms.org>
2019-09-23 10:49:00 -0400

    only our repos

--- script/download-packagecloud.sh
+++ script/download-packagecloud.sh
@@ -92,7 +92,7 @@ END
   rpm-docker)
     # make sure the cache is 100% up-to-date
     yum -y --verbose clean all
-    yum -y --verbose list
+    yum -y --verbose --disablerepo='*' --enablerepo="$REPOID" --enablerepo="$REPOID-source" list
     reposync --allow-path-traversal --delete --repoid="$REPOID" --download_path=/repo/ --urls
     #reposync --allow-path-traversal --delete --repoid="$REPOID-source" --download_path=/repo/ --urls
     reposync --allow-path-traversal --delete --repoid="$REPOID" --download_path=/repo/
