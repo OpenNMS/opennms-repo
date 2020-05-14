4b4ee03e332cb5a28e60a3857bb74cc41840be30
Benjamin Reed <ranger@opennms.org>
2020-05-14 09:54:41 -0400

    allow setting ownership

--- script/sync-packagecloud.sh
+++ script/sync-packagecloud.sh
@@ -1,8 +1,11 @@
-#!/bin/sh -e
+#!/bin/sh
+
+set -e
 
 CACHEDIR="$1"; shift || :
 YUM_REPODIR="$1"; shift || :
 APT_REPODIR="$1"; shift || :
+OWNER="$1"; shift || :
 
 MYDIR="$(dirname "$0")"
 MYDIR="$(cd "$MYDIR"; pwd)"
@@ -10,12 +13,17 @@ MYDIR="$(cd "$MYDIR"; pwd)"
 DL="$MYDIR/download-packagecloud.sh"
 RM="$MYDIR/remove-obsolete-rpms.sh"
 
-mkdir -p "$CACHEDIR"
+if [ -z "$OWNER" ]; then
+  OWNER="bamboo:repo"
+fi
 
 if [ -z "$APT_REPODIR" ]; then
-  echo "usage: $0 <cachedir> <yum_repodir> <apt_repodir>"
+  echo "usage: $0 <cachedir> <yum_repodir> <apt_repodir> [owner]"
+  echo ""
   exit 1
 fi
+
+mkdir -p "$CACHEDIR"
 
 sign_file() {
   "$MYDIR/sign-package.pl" "$1"
@@ -63,22 +71,22 @@ deb_sign_unsigned() {
   done
 }
 
-"$DL" "rpm" "opennms/plugin-stable"   "$CACHEDIR/rpm/stable"
+"$DL" "rpm" "opennms/plugin-stable" "$CACHEDIR/rpm/stable" "$OWNER"
 install -d "$YUM_REPODIR/stable/common/packagecloud/"
-rsync -al --ignore-existing --no-compress "$CACHEDIR/rpm/stable/" "$YUM_REPODIR/stable/common/packagecloud/"
+rsync -al --ignore-existing "$CACHEDIR/rpm/stable/" "$YUM_REPODIR/stable/common/packagecloud/"
 
-"$DL" "rpm" "opennms/plugin-snapshot" "$CACHEDIR/rpm/snapshot"
-"$RM" "opennms_plugin-snapshot" "$CACHEDIR/rpm/snapshot"
+"$DL" "rpm" "opennms/plugin-snapshot" "$CACHEDIR/rpm/snapshot" "$OWNER"
+"$RM" "opennms_plugin-snapshot" "$CACHEDIR/rpm/snapshot" || :
 install -d "$YUM_REPODIR/bleeding/common/packagecloud/"
-rsync -al --ignore-existing --no-compress --delete "$CACHEDIR/rpm/snapshot/" "$YUM_REPODIR/bleeding/common/packagecloud/"
+rsync -al --ignore-existing --delete "$CACHEDIR/rpm/snapshot/" "$YUM_REPODIR/bleeding/common/packagecloud/"
 
-"$DL" "deb" "opennms/plugin-stable"   "$CACHEDIR/deb/stable"
+"$DL" "deb" "opennms/plugin-stable"   "$CACHEDIR/deb/stable" "$OWNER"
 install -d "$APT_REPODIR/dists/stable/main/binary-all/packagecloud/"
-rsync -al --ignore-existing --no-compress "$CACHEDIR/deb/stable/mirror/packagecloud.io/opennms/plugin-stable/debian/pool/stretch/main/" "$APT_REPODIR/dists/stable/main/binary-all/packagecloud/"
+rsync -al --ignore-existing "$CACHEDIR/deb/stable/mirror/packagecloud.io/opennms/plugin-stable/debian/pool/stretch/main/" "$APT_REPODIR/dists/stable/main/binary-all/packagecloud/"
 
-"$DL" "deb" "opennms/plugin-snapshot" "$CACHEDIR/deb/snapshot"
+"$DL" "deb" "opennms/plugin-snapshot" "$CACHEDIR/deb/snapshot" "$OWNER"
 install -d "$APT_REPODIR/dists/bleeding/main/binary-all/packagecloud/"
-rsync -al --ignore-existing --no-compress --delete "$CACHEDIR/deb/snapshot/mirror/packagecloud.io/opennms/plugin-snapshot/debian/pool/stretch/main/" "$APT_REPODIR/dists/bleeding/main/binary-all/packagecloud/"
+rsync -al --ignore-existing --delete "$CACHEDIR/deb/snapshot/mirror/packagecloud.io/opennms/plugin-snapshot/debian/pool/stretch/main/" "$APT_REPODIR/dists/bleeding/main/binary-all/packagecloud/"
 
 rpm_sign_unsigned "$YUM_REPODIR/stable/common/packagecloud"
 rpm_sign_unsigned "$YUM_REPODIR/bleeding/common/packagecloud"
